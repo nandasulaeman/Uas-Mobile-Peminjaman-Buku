a05e5a0f783fa61d1f45865b11ee97fd4ee8c607
nandasulaeman0312 <48647543+nandasulaeman@users.noreply.github.com>
2019-06-21 21:20:23 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Tab1 from "./src/UAS/Tab/Tab1";
+import Tab2 from "./src/UAS/Tab/Tab2";
+import Category from "./src/UAS/Category";
+import About from "./src/UAS/About";
+import Main from './src/UAS/Main.js'
+import AppNavigator from "./src/UAS/AppNavigator";
+export default class App extends Component {
+  render() {
+    return (
+      <Main />
+    )
+  }
+}
